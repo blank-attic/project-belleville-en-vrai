805d8527459c6c9b1c57de6b1706f4b252154b02
Laurent Abemango <abemango.laurent@gmail.com>
2017-03-10 15:04:26 +0100

    index avec require beneform

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>Belleville En Vrai</title>
+  <link rel="stylesheet" href="assets/css/ddtstyle.css" type="text/css">
+</head>
+<body>
+  <header id="hdrndx">
+  </header>
+  <h1>BELLEVILLE EN VRAI 8 a besoin de TOI !</h1>
+  <h2>Pour sa 8ème édition (et oui déja !!!) qui aura lieu du 19 au 21 Mai 2017,<br/> BELLEVILLE EN VRAI, le Festival de Belleville RECRUTE des Bénévoles !!</h2>
+  <h1>REJOINS NOUS... </h1>
+  <h2>...Construisons 3 Jours de Fête, ENSEMBLE !</h2>
+  <?php
+  require ("beneform.php");
+   ?>
+</body>
+</html>
