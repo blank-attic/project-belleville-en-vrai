2066c12596a56b0babdf6d718e09324c89bce882
Badis Nakhli <nakhli.badis@gmail.com>
2017-03-20 18:08:53 +0100

    ajout d'une page de remerciement pour les benevoles

---
+++ merci.php
@@ -0,0 +1,24 @@
+<?php
+require_once('_db.php');
+if (!isset($_GET["firstname"])) {
+  header("location:index.php");
+}
+?>
+
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="assets/css/ddtstyle.css">
+    <title>MERCI !!!</title>
+  </head>
+  <body>
+    <div class="thxmsg">
+
+      <?php
+        echo "<h1 id='thxmsg'>  Je te remercie pour ta contribution ".$_GET["firstname"]."  pour toute autre information tu peux toujours nous contacter sur : participation@bellevillenvrai.fr </h1>";
+       ?>
+     </div>
+
+  </body>
+</html>
