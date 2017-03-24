f50a9c56f9a31bba88e0a897e107e8a3d7b9ccea
Marin Procureur <marin.procureur@gmail.com>
2017-03-24 19:13:59 +0100

    Add teams view

--- class/PlayerDAO.php
+++ class/PlayerDAO.php
@@ -2,48 +2,63 @@
 
 class PlayerDAO {
 
-  //save in db
-  public function save($db, $player) {
+    //save in db
+    public function save($db, $player) {
 
-    //prepare request
-    $request = $db->prepare("INSERT INTO player (name, tel, bdate, team_id) VALUES (:name, :tel, :bdate, :team_id)");
+        //prepare request
+        $request = $db->prepare("INSERT INTO player (name, tel, bdate, team_id, team_leader) VALUES (:name, :tel, :bdate, :team_id, :team_leader)");
 
-    //execute request
-    try {
-      $request->execute(array(
-        "team_id" => $player->getTeamId(),
-        "name" => $player->getName(),
-        "tel" => $player->getTel(),
-        "bdate" => $player->getDate(),
-      ));
-    } catch (PDOException $e) {
-      print("error while writing in DB new player." . $e->getMessage());
-      return false;
+        //execute request
+        try {
+            $request->execute(array(
+                "team_id" => $player->getTeamId(),
+                "name" => $player->getName(),
+                "tel" => $player->getTel(),
+                "bdate" => $player->getDate(),
+                "team_leader" => $player->getTeamLeader(),
+            ));
+        } catch (PDOException $e) {
+            print("error while writing in DB new player." . $e->getMessage());
+            return false;
+        }
+        return true;
     }
-    return true;
+
+    public function find($db, $team_id, $name, $tel, $bdate, $team_leader) {
+
+        //prepare request
+        $request = $db->prepare("SELECT * FROM player WHERE (team_id = :team_id) AND (name = :name) AND (tel = :tel) AND (bdate = :bdate) AND (team_leader = :team_leader) LIMIT 1");
+
+        //execute request
+        try {
+            $request->execute(array(
+                "team_id" => $team_id,
+                "name" => $name,
+                "tel" => $tel,
+                "bdate" => $bdate,
+                "team_leader" => $team_leader
+            ));
+
+            return $request->fetch();
+        } catch (PDOException $e) {
+            print("error while writing in DB new player." . $e->getMessage());
+            return false;
+        }
     }
 
-  public function find($db, $team_id, $name, $tel, $bdate, $team_leader) {
+    public function getAllPlayersAndTeam($db) {
+        //prepare request
+        $request = $db->prepare("SELECT * FROM team t JOIN player p where p.team_id = t.id");
 
-    //prepare request
-    $request = $db->prepare("SELECT * FROM player WHERE (team_id = :team_id) AND (name = :name) AND (tel = :tel) AND (bdate = :bdate) AND (team_leader = :team_leader) LIMIT 1");
-
-    //execute request
-    try {
-      $request->execute(array(
-        "team_id" => $team_id,
-        "name" => $name,
-        "tel" => $tel,
-        "bdate" => $bdate,
-        "team_leader" => $team_leader
-      ));
-
-      return $request->fetch();
-    } catch (PDOException $e) {
-      print("error while writing in DB new player." . $e->getMessage());
-      return false;
+        //execute request
+        try {
+            $request->execute();
+            return $request->fetchAll();
+        } catch (PDOException $e) {
+            print("error while writing in DB new volunteer." . $e->getMessage());
+            return false;
+        }
     }
-  }
 }
 
- ?>
+?>
