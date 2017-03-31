2e2e81a86e0d5aabe2277e0ccb06f0e026bb323c
Badis Nakhli <nakhli.badis@gmail.com>
2017-03-31 17:27:36 +0200

    remplacement de loge artiste par Live + modification  flag

---
+++ benevoles.php
@@ -0,0 +1,89 @@
+<?php
+require_once("_db.php");
+
+spl_autoload_register(function ($class) {
+  include 'class/' . $class . '.php';
+});
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Inscription Bénévole</title>
+  <link rel="stylesheet" href="assets/css/sportformstyle.css" type="text/css">
+  <link href="https://fonts.googleapis.com/css?family=Sansita" rel="stylesheet">
+</head>
+<body>
+  <h1>LISTE DE BENEVOLES BEV 8 (2017) :</h1>
+  <div class="space">
+  </div>
+  <div class="bluebg">
+    <?php
+    //create dao
+    $dao = new VolunteerDAO();
+    //get all volunteers
+    $volunteers = $dao->getAll($_DB);
+    echo "<table class='volunteers'>";
+    echo "<thead>";
+    echo "<tr>";
+    echo "<th>Prénom</th>";
+    echo "<th>Nom</th>";
+    echo "<th>Tel</th>";
+    echo "<th>Email</th>";
+    echo "<th>TShirt</th>";
+    echo "<th>Vendredi 19 mai (18h-21h)</th>";
+    echo "<th>Samedi 20 mai (8h-13h)</th>";
+    echo "<th>Samedi 20 mai (13h-20h)</th>";
+    echo "<th>Dimanche 21 mai (8h-13h)</th>";
+    echo "<th>Dimanche 21 mai (13h-20h)</th>";
+    echo "<th>Encadrement Sportif</th>";
+    echo "<th>Logistique</th>";
+    echo "<th>Photo</th>";
+    echo "<th>Vidéo</th>";
+    echo "<th>Live</th>";
+    echo "<th>Cuisine</th>";
+    echo "<th>Accueil/Propreté</th>";
+    echo "<th>Commentaires</th>";
+    echo "</tr>";
+    echo "<tbody>";
+    foreach ($volunteers as $k => $v) {
+      echo "<tr class='volunteer'>";
+      echo "<td>$v[prenom]</td>";
+      echo "<td>$v[nom]</td>";
+      echo "<td>$v[tel]</td>";
+      echo "<td>$v[email]</td>";
+      echo "<td>$v[taille_tshirt]</td>";
+
+      //dispos with bitwise flags
+      $dispos = new Dispo(array());
+      $dispos->setFlags($v["dispo"]);
+      echo ($dispos->isVendredi() ? "<td class='bigx'>x</td>" : "<td></td>");
+      echo ($dispos->isSamediAM() ? "<td class='bigx'>x</td>" : "<td></td>");
+      echo ($dispos->isSamediPM() ? "<td class='bigx'>x</td>" : "<td></td>");
+      echo ($dispos->isDimancheAM() ? "<td class='bigx'>x</td>" : "<td></td>");
+      echo ($dispos->isDimanchePM() ? "<td class='bigx'>x</td>" : "<td></td>");
+
+      //task with bitwise flags
+      $tasks = new Task(array());
+      $tasks->setFlags($v["tache"]);
+      echo ($tasks->isEncadrementSportif() ? "<td>x</td>" : "<td></td>");
+      echo ($tasks->isLogistique() ? "<td class='bigx'>x</td>" : "<td></td>");
+      echo ($tasks->isPhoto() ? "<td class='bigx'>x</td>" : "<td></td>");
+      echo ($tasks->isVideo() ? "<td class='bigx'>x</td>" : "<td></td>");
+      echo ($tasks->isLive() ? "<td class='bigx'>x</td>" : "<td></td>");
+      echo ($tasks->isCuisine() ? "<td class='bigx'>x</td>" : "<td></td>");
+      echo ($tasks->isAccueilProprete() ? "<td class='bigx'>x</td>" : "<td></td>");
+      echo "<td>$v[message]</td>";
+
+      echo "</tr>";
+    }
+
+    echo "</tbody>";
+    echo "</table>";
+    ?>
+  </div>
+</body>
+</html>
---
+++ class/Task.php
@@ -0,0 +1,94 @@
+<?php
+require_once('BitwiseFlag.php');
+
+class Task extends BitwiseFlag
+{
+  const FLAG_ENCADREMENT_SPORTIF = 1; // BIT #1 of $flags has the value 1
+  const FLAG_LOGISTIQUE = 2;     // BIT #2 of $flags has the value 2
+  const FLAG_PHOTO = 4;     // BIT #4 of $flags has the value 4
+  const FLAG_VIDEO = 8;      // BIT #8 of $flags has the value 8
+  const FLAG_CUISINE = 16;      // BIT #16 of $flags has the value 16
+  const FLAG_LIVE = 32;      // BIT #32 of $flags has the value 32
+  const FLAG_ACCEUIL_PROPRETE = 64;      // BIT #64 of $flags has the value 64
+
+  public function __construct($task) {
+    parent::__construct();
+    foreach($task as $key => $value) {
+      $this->setFlag($value, TRUE);
+    }
+  }
+
+
+  public function isEncadrementSportif(){
+    return $this->isFlagSet(self::FLAG_ENCADREMENT_SPORTIF);
+  }
+
+  public function isLogistique(){
+    return $this->isFlagSet(self::FLAG_LOGISTIQUE);
+  }
+
+  public function isPhoto(){
+    return $this->isFlagSet(self::FLAG_PHOTO);
+  }
+
+  public function isVideo(){
+    return $this->isFlagSet(self::FLAG_VIDEO);
+  }
+
+  public function isCuisine(){
+    return $this->isFlagSet(self::FLAG_CUISINE);
+  }
+
+  public function isLive(){
+    return $this->isFlagSet(self::FLAG_LIVE);
+  }
+
+  public function isAccueilProprete(){
+    return $this->isFlagSet(self::FLAG_ACCEUIL_PROPRETE);
+  }
+
+
+  public function setEncadrementSportif($value){
+    $this->setFlag(self::FLAG_ENCADREMENT_SPORTIF, $value);
+  }
+
+  public function setLogistique($value){
+    $this->setFlag(self::FLAG_LOGISTIQUE, $value);
+  }
+
+  public function setPhoto($value){
+    $this->setFlag(self::FLAG_PHOTO, $value);
+  }
+
+  public function setVideo($value){
+    $this->setFlag(self::FLAG_VIDEO, $value);
+  }
+
+  public function setCuisine($value){
+    $this->setFlag(self::FLAG_CUISINE, $value);
+  }
+
+  public function setLive($value){
+    $this->setFlag(self::FLAG_LIVE, $value);
+  }
+
+  public function setAcceuilProprete($value){
+    $this->setFlag(self::FLAG_ACCEUIL_PROPRETE, $value);
+  }
+
+  public function getTask() {
+    $taskTab = array('Encadrement Sportif' =>getflag(self::FLAG_ENCADREMENT_SPORTIF),
+    'Logistique'=>getflag(self::FLAG_LOGISTIQUE),
+    'Photo'=>getflag(self::FLAG_PHOTO),
+    'Video'=>getflag(self::FLAG_VIDEO),
+    'Cuisine'=>getflag(self::FLAG_CUISINE),
+    'Live'=>getflag(self::FLAG_LIVE),
+    'Acceuil Propreté'=>getflag(self::FLAG_ACCEUIL_PROPRETE));
+  }
+
+  public function __toString(){
+    return $this->getTask();
+
+  }
+}
+?>
