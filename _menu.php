f3d12feb8bb4abc3826916c5d7f320c9679506ac
Laurent Abemango <majorgrafismz@gmail.com>
2017-04-03 15:28:30 +0200

    menu started

---
+++ _menu.php
@@ -0,0 +1,23 @@
+<div class="mainmenu">
+<nav>
+  <ul>
+    <li>
+      <a href="http://bellevillenvrai.fr/inscriptions/">Participer</a>
+    </li>
+    <li>
+      <a href="programme.php">Programme</a>
+    </li>
+    <li>
+      <a href="index.php">
+        <img src="" alt="">
+      </a>
+    </li>
+    <li>
+
+    </li>
+    <li>
+
+    </li>
+  </ul>
+</nav>
+</div>
