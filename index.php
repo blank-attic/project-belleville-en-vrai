1c332cf69e43af8075d51336312482acce0b524e
Laurent Abemango <majorgrafismz@gmail.com>
2017-04-04 11:06:10 +0200

    some air in the index...

--- index.php
+++ index.php
@@ -19,17 +19,24 @@
   <?php
   require ('car.php');
   ?>
+  <div class="space"></div>
   <div class="txtbg">
     <h1>BELLEVILLE EN VRAI 8 C'est BIENTOT !</h1>
+    <div class="space"></div>
     <h2>UN TOURNOI JEUNE, SPORTIF ET FESTIF</h2>
+    <div class="space"></div>
     <p>BELLEVILLE EN VRAI est avant tout un tournoi sportif puisqu’il propose aux jeunes de quatre quartiers du Grand Belleville (Rébeval – Ramponneau – Orillon – Piat) de s’affronter par équipes autour de cinq sports différents.</p>
+    <div class="space"></div>
     <h3>Au Programme :</h3>
-    <p>Du Football, de la Natation, du Tennis de Table, du Cross et du Basket.</p>
-    <p>S’ajoute à cela un quizz de culture générale pour également muscler ses méninges.</p>
+    <p>Du Football, de la Natation, du Tennis de Table, du Cross et du Basket.
+      S’ajoute à cela un quizz de culture générale pour également muscler ses méninges.</p>
+    <div class="space"></div>
     <P>Chaque année l’événement rassemble près de 500 jeunes entre 10 et 25 ans, garçons et filles, répartis en équipe de huit et en trois catégories : les petit.e.s (10-13 ans), les moyen.ne.s (14-17 ans) et les grand.e.s (18-25 ans).</p>
       <p>Pendant deux jours, les jeunes de chaque équipe doivent gagner un maximum de points par épreuve pour prétendre au titre honorifique de :</p>
       <h2>Champions ou Championnes du Quartier !</h2>
+      <div class="space"></div>
       <h2>...Construisons 3 Jours de Fête, ENSEMBLE !</h2>
+      <div class="space"></div>
       <div class="formlinks">
         <a href="beneform.php">INSCRIPTION BENEVOLE</a>
         <a href="sportform.php">INSCRIPTION TOURNOI</a>
