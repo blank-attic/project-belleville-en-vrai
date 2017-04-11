8a0fb499470945b4d94cd1787543f59d4f1e513b
Laurent Abemango <majorgrafismz@gmail.com>
2017-04-11 16:20:48 +0200

    bevquoi done

---
+++ quoi.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta name="description" content="ce site est une présentation du Festival du Quartier BELLEVILLE, Belleville En Vrai"/>
+  <meta name="author" content="Laurent Abemango alias LAWD / Badis Nakhli / Abdoulaye Ndao"/>
+  <title>BELLEVILLE EN VRAI</title>
+  <link href="https://fonts.googleapis.com/css?family=Sansita" rel="stylesheet">
+  <link href="assets/vendor/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+  <link rel="stylesheet" href="assets/css/ddtstyle.css" type="text/css">
+  <link rel="stylesheet" href="assets/css/menustyle.css" type="text/css">
+  <link rel="stylesheet" href="assets/css/mqstyle.css" media="screen" type="text/css">
+</head>
+  <body>
+    <?php
+      require('_menu.php');
+     ?>
+    <div class="txtbg">
+      <div>
+        <h1>BELLEVILLE EN VRAI C'EST :</h1>
+        <h2>UN TOURNOI JEUNE, SPORTIF ET FESTIF !!!</h2>
+        <div class="space"></div>
+        <p><span class="titp">Belleville en vrai </span>est avant tout un tournoi sportif puisqu’il propose aux jeunes de quatre quartiers  du Grand Belleville (Rébeval – Ramponneau – Orillon – Piat) de s’affronter par équipes autour de cinq sports différents. </p>
+        <div class="space"></div>
+        <p><span class="titp">Au programme : </span>football, natation, tennis de table, cross et basket. S’ajoute à cela un quizz de culture générale pour également muscler ses méninges. </p>
+        <div class="space"></div>
+        <p>Chaque année l’événement rassemble près de 500 jeunes entre 10 et 25 ans, garçons et filles, répartis en équipe de huit et en trois catégories : les petit.e.s (10-13 ans), les moyen.ne.s (14-17 ans) et les grand.e.s (18-25 ans). Pendant deux jours, les jeunes de chaque équipe doivent gagner un maximum de points par épreuve pour prétendre au titre honorifique de <br/><span class="titp">champions ou championnes du quartier !</span></p>
+      </div>
+    </div>
+    <?php
+      require('_footer.php');
+     ?>
+  </body>
+</html>
