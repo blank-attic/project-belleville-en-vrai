75e0468edf3003b34b0c0660f9636a79bd421e6c
Laurent Abemango <majorgrafismz@gmail.com>
2017-04-14 18:37:35 +0200

    rajout menu sur gallery

---
+++ _menu.php
@@ -0,0 +1,23 @@
+<div class="mainmenu">
+<nav>
+  <ul>
+    <li>
+      <a href="http://bellevillenvrai.fr/inscriptions/" class="menutabz"><i class="fa fa-user-plus" aria-hidden="true"></i> PARTICIPER</a>
+    </li>
+    <li>
+      <a href="programme.php" class="menutabz"><i class="fa fa-list-alt" aria-hidden="true"></i> PROGRAMME</a>
+    </li>
+    <li class="ndxbutt">
+      <a href="index.php">
+        <img src="assets/images/bev_butt1.png" alt="logo_bev 8" class="ndxbutt">
+      </a>
+    </li>
+    <li>
+      <a href="bevtv.php" class="menutabz"><i class="fa fa-television" aria-hidden="true"></i> BEV TV</a>
+    </li>
+    <li>
+      <a href="gallery.php" class="menutabz"><i class="fa fa-picture-o" aria-hidden="true"></i> GALERIE</a>
+    </li>
+  </ul>
+</nav>
+</div>
---
+++ contact.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta name="description" content="ce site est une présentation du Festival du Quartier BELLEVILLE, Belleville En Vrai"/>
+  <meta name="author" content="Laurent Abemango alias LAWD / Badis Nakhli / Abdoulaye Ndao"/>
+  <title>BELLEVILLE EN VRAI</title>
+  <link href="https://fonts.googleapis.com/css?family=Sansita" rel="stylesheet">
+  <link href="assets/vendor/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+  <link rel="stylesheet" href="assets/css/ddtstyle.css" type="text/css">
+  <link rel="stylesheet" href="assets/css/menustyle.css" type="text/css">
+  <link rel="stylesheet" href="assets/css/mqstyle.css" media="screen" type="text/css">
+</head>
+  <body>
+    <div>
+      <?php
+        require('_menu.php');
+       ?>
+    </div>
+    <div class="txtbg">
+    <div>
+      <h1>Pour plus d'information, contactez nous :</h1>
+      <h2><a class="contlink2" href="mailto:participation@bellevillenvrai.fr"><i class="fa fa-envelope" aria-hidden="true"></i>   participation@bellevillenvrai.fr</a></h2>
+      <h2><a class="contlink" href="https://www.facebook.com/pg/Belleville-en-Vrai-167646066632163"><i class="fa fa-facebook-square" aria-hidden="true"></i>    facebook</a></h2>
+      <h2><a class="contlink" href="https://twitter.com/blvenvrai"><i class="fa fa-twitter-square" aria-hidden="true"></i>    twitter</a></h2>
+  </body>
+</html>
