aa83fcd2f7724f91ec91b50a83f5629681adeb01
Laurent Abemango <majorgrafismz@gmail.com>
2017-05-10 17:53:06 +0200

    modifs programme et carto a toi de jouer GABLO ;)

--- _carto.php
+++ _carto.php
@@ -26,22 +26,26 @@
 });
 
   L.marker([48.8714538,2.3788977], {icon: blackIcon}).addTo(mymap)
-  .bindPopup('Vendredi 19 Mai<h3>Piscine A.Nakache</h3>18h00: Epreuve de Natation<br/>Métro: Belleville')
+  .bindPopup('<h3>Piscine A.Nakache</h3>Vendredi 19 Mai<br/>18h00: Epreuve de Natation<br/>Métro: Belleville')
   .openPopup();
   L.marker([48.871489,2.378383], {icon: blackIcon}).addTo(mymap)
-  .bindPopup('Vendredi 19 Mai<h3>19h00:<br/> SURPRISES !!!</h3>Métro: Belleville');
+  .bindPopup('<h3>Vendredi 19 Mai 19h00:<br/> SURPRISES !!!</h3>Métro: Belleville');
   L.marker([48.8710678,2.3847051], {icon: blackIcon}).addTo(mymap)
-  .bindPopup('Samedi 20 Mai<h3>Parc de Belleville</h3>09h30: Epreuves du Cross <br/>Dimanche 21 Mai<br/>14h00-17h00 : Animation Foot<br/>Métro: Belleville, Couronnes, Pyrénées');
+  .bindPopup('<h3>Parc de Belleville</h3>Samedi 20 Mai<br/>09h30: Epreuves du Cross <br/>Dimanche 21 Mai<br/>14h00-17h00 : Animation Sportive par Tatane Football<br/>Métro: Belleville, Couronnes, Pyrénées');
   L.marker([48.87064,2.382127], {icon: blackIcon}).addTo(mymap)
-  .bindPopup('Samedi 20 Mai<h3>Place Alphonse Allais</h3>09h00: Chantier participatif<br/>12h00: Scène d’expression libre<br/>15h00 : Conférence gesticulée<br/>16h30 : Goûter partagé<br/>18h00 : Scène Artistes Quartier<br/>Métro: Belleville, Couronnes');
+  .bindPopup('<h3>Place Alphonse Allais</h3>Samedi 20 Mai<br/>10h00: Chantier participatif<br/>15h00 : Débats<br/>18h00 : Scène Ouverte<br/>Métro: Belleville, Couronnes');
   L.marker([48.870012, 2.376315], {icon: blackIcon}).addTo(mymap)
-  .bindPopup('Dimanche 21 Mai<h3>Gymnase Orillon</h3>10h00: Finales Basket<br/>Métro: Belleville');
-  L.marker([48.8754096,2.3779683], {icon: blackIcon}).addTo(mymap)
-  .bindPopup('Dimanche 21 Mai<h3>TEP Jandelle</h3>12h00: Finales Foot<br/>Métro: Belleville');
-  L.marker([48.8715766,2.385567], {icon: blackIcon}).addTo(mymap)
-  .bindPopup('Dimanche 21 Mai<h3>Belvédère du Parc de Belleville</h3>Ateliers participatifs<br/>Expositions<br/>Métro: Pyrénées');
-  L.marker([48.871587,2.385070], {icon: blackIcon}).addTo(mymap)
-  .bindPopup('Dimanche 21 Mai<h3>Amphithéatre du Parc de Belleville</h3>16h00 : Spectacle Danse Contemporaine<br/>17h00-20h00 : Concerts<br/>20h00 : Remise des Trophées<br/>Métro: Pyrénées');
+  .bindPopup('<h3>Gymnase Orillon</h3>Dimanche 21 Mai<br/>10h00: Finales Basket<br/>Métro: Belleville');
+  L.marker([48.8753076,2.3795783], {icon: blackIcon}).addTo(mymap)
+  .bindPopup('<h3>TEP Jandelle</h3>Samedi 20 Mai<br/>12h00-19h00: Qualifications Foot<br/><br/>Dimanche 21 Mai <br/>12h00: Finales Foot<br/>14h00: Match amical football féminin <br/><br/>Métro: Belleville');
+  L.marker([48.8715766,2.385230], {icon: blackIcon}).addTo(mymap)
+  .bindPopup('<h3>Belvédère du Parc de Belleville</h3>Dimanche 21 Mai<br/>Ateliers participatifs<br/>Strataj’m : atelier de convivialité et de jeux pour petits et grands<br/>Curry Vavart – Villa Belleville : atelier de construction participatif de passes-têtes et d’assises<br/>Catharsis : Cabine d’expression vidéo<br/>Point temporaire d’accès au droit<br/>Espaces de restauration<br/>Métro: Pyrénées');
+  L.marker([48.871687,2.384900], {icon: blackIcon}).addTo(mymap)
+  .bindPopup('<h3>Amphithéatre du Parc de Belleville</h3>Dimanche 21 Mai<br/>15h00: JEUX DE LA JONGLE – Spectacle participatif de jonglerie<br/>16h00 : OUVERTURE – Performances et spectacles de danse<br/>17h00: CONCERTS ET REMISE DES PRIX<br/><h4>BLVL MAFIA | MAKADERO | 19 RESEAUX | CHILLA | LINO</h4><br/>Métro: Pyrénées');
+  L.marker([48.869659,2.378683], {icon: blackIcon}).addTo(mymap)
+  .bindPopup('<h3>Gymnase Fontaine au Roi</h3>Samedi 20 Mai<br/>11h00-19h00: Qualifications Basket | Ping Pong | Foot<br/>100 rue de la Fontaine au roi | Métro: Belleville');
+  L.marker([48.870152,2.385056], {icon: blackIcon}).addTo(mymap)
+  .bindPopup('<h3>Salle polyvalente Hervé Rozental</h3>Samedi 20 Mai<br/>11h00-19h00: Qualifications Basket | Ping Pong | Foot<br/>64 rue des Couronnes | Métro: Couronnes');
 
 
   </script>
--- programme.php
+++ programme.php
@@ -22,6 +22,7 @@
   <div class="prog">
     <h1>VENDREDI 19 MAI</h1>
     <div class="progtxt">
+      <h2><span class="orange">SPORT</span></h2>
       <h2><span class="yello">18h :</span> Epreuve de Natation - Piscine Alfred Nakache</h2>
       <h3>4-12 Rue Dénoyez | Métro : Belleville</h3>
 
@@ -29,52 +30,46 @@
       <h3>Rue Dénoyez | Métro : Belleville</h3>
     </div>
     <div class="space"></div>
-    <h1>SAMEDI 20 MAI</h1>
     <div class="progtxt">
+    <h1>SAMEDI 20 MAI</h1>
+    <h2><span class="orange">SPORT</span></h2>
       <h2><span class="yello">9h30 :</span> Epreuve de Cross</h2>
       <h3>Parc de Belleville | Métro : Belleville, Couronnes, Pyrénées</h3>
       <h2><span class="yello">13h - 20h :</span> Qualifications :</h2>
       <h3>foot, basket, tennis de table et quizz de culture générale</h3>
       <div class="space"></div>
-      <h2><span class="yello">9h :</span> Chantier Participatif :</h2>
-      <h3>Construction collective de la scénographie</h3>
-      <h2><span class="yello">12h :</span> Scène d’Expression Libre</h2>
-      <h2><span class="yello">14h :</span> Animation Sportive</h2>
-      <h2><span class="yello">17h :</span> Conférence gesticulée / Débat</h2>
-      <h2><span class="yello">18h - 21h :</span> Scène quartier : scène amateur ouverte via <a href="partiform.php"class="contlink">- Appel à Participation -</a></h2>
-      <h2>Toute la journée, retransmission Live des Epreuves et des Scores, point temporaire d’accès au Droit</h2>
-      <h3>Place Alphonse Allais | Métro : Belleville, Couronnes</h3>
+      <h2><span class="orange">CULTURE | Place Alphonse Allais</span></h2>
+      <h2><span class="yello">10h :</span> Chantier Participatif :</h2>
+      <h2><span class="yello">15h :</span> Débats</h2>
+      <h2><span class="yello">18h :</span> Scène Ouverte via <a href="partiform.php"class="contlink">- Appel à Participation -</a></h2>
+      <h3>Métro : Belleville, Couronnes</h3>
     </div>
     <div class="space"></div>
     <h1>DIMANCHE 21 MAI</h1>
     <div class="progtxt">
-      <h2><span class="yello">10h :</span> Finales Basket - Gymnase Maurice Berlémont</h2>
-      <h3>22 Rue de l'Orillon | Métro : Belleville</h3>
-
-      <h2><span class="yello">12h :</span> Finales Foot - TEP Jandelle</h2>
+      <h2><span class="orange">SPORT | TEP Jandelle</span></h2>
+      <h2><span class="yello">12h :</span> Finales Foot</h2>
+      <h2><span class="yello">14h :</span> Match amical football féminin avec Les Dégommeuses.</h2>
       <h3>15 cité Jandelle | Métro : Belleville</h3>
-
-      <h2><span class="yello">15h30 - 16h00 :</span> Batucada - Déambulation</h2>
-      <h3>du TEP Jandelle à l'Amphithéâtre du Parc de Belleville</h3>
     </div>
     <div class="space"></div>
 
-    <h2 class="yello">Au Belvédère du Parc de Belleville</h2>
+    <h2 class="yello">Ateliers Participatifs | Au Belvédère du Parc de Belleville</h2>
     <div class="progtxt">
-      <h2>Village Associatif – Ateliers Participatifs</h2>
+      <h2>Strataj’m : atelier de convivialité et de jeux pour petits et grands</h2>
+      <h2>Curry Vavart – Villa Belleville : atelier de construction participatif de passes-têtes et d’assises</h2>
+      <h2>Catharsis : Cabine d’expression vidéo</h2>
+      <h2>Tatane Football - Animation sportive sur les grandes pelouses du parc à partir de 14h.</h2>
+      <h2>Point temporaire d’accès au droit</h2>
       <h3>Rue Piat | Métro : Pyrénées</h3>
     </div>
     <div class="space"></div>
-    <h2 class="yello">A l'Amphithéâtre du Parc de Belleville </h2>
+    <h2 class="yello">Concerts & animations | A l'Amphithéâtre du Parc de Belleville </h2>
     <div class="progtxt">
-      <h2><span class="yello">15h :</span> Jeux de la Jongle – Spectacle participatif</h2>
-      <h2><span class="yello">16h :</span> Démonstration arts martiaux & Démonstration Double Dutch</h2>
-      <h2><span class="yello">16h30 :</span> Juste Debout – Spectacle de danse</h2>
-      <h2><span class="yello">17h :</span> 1ère partie – artistes confirmés du Quartier</h2>
-      <h2><span class="yello">18h :</span> Concert de hip-hop</h2>
-      <h2><span class="yello">19h :</span> Concert de hip-hop</h2>
-      <h2><span class="yello">20h :</span> Remise des trophées par les parrains et marraines et clôture de l’événement</h2>
-
+      <h2><span class="yello">15h :</span> JEUX DE LA JONGLE – Spectacle participatif de jonglerie</h2>
+      <h2><span class="yello">16h :</span> OUVERTURE – Performances et spectacles de danse</h2>
+      <h2><span class="yello">17h :</span> CONCERTS ET REMISE DES PRIX</h2>
+      <h2>BLVL MAFIA - MAKADERO – 19 RESEAUX – CHILLA – LINO</h2>
       <h3>Rue Piat | Métro : Pyrénées</h3>
     </div>
   </div>
