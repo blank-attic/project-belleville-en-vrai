62feaa8b8d274dc96725e8cd4ebb648ef346536e
Laurent Abemango <majorgrafismz@gmail.com>
2017-05-23 16:00:12 +0200

    remerciements bev

--- index.php
+++ index.php
@@ -19,13 +19,21 @@
       require('_menu.php');
      ?>
   </div>
+  <div class="txtbg">
+    <h1 class="yello">U N &nbsp &nbspT R E S &nbsp &nbsp  G R A N D &nbsp &nbsp    M E R C I  &nbsp &nbsp   ! ! ! ! !</h1>
+    <h2>A tous les Participants, à tous les Bénévoles, à tous les Artistes, à tous les Partenaires et à tous ceux qui sont venus :</h2>
+    <h1> <span class="yello">M&nbsp &nbsp E&nbsp &nbsp R&nbsp &nbsp C&nbsp &nbsp I&nbsp &nbsp ! ! !</span> </h1>
+
+    <h1>A l'année prochaine pour une nouvelle édition encore plus belle !<span class="yello"> *</span></h1>
+    <h3>Partagez vos films et vos photos du Festival avec le hashtag :<span class="yello"> #bev</span></h3>
+    <h3>Et Envoyez les à l'adresse suivante : <span class="yello">h.bellevillenvrai[at]gmail.com</span> pour la video récap' </h3>
+    <h5><span class="yello">* si c'est possible !!</span></h5>
+  </div>
 
     <div>
-<iframe class="vidprez" src="https://player.vimeo.com/video/211642278?color=fff600&title=0&byline=0&portrait=0" width="1180" height="664" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
+<iframe class="vidprez" src="https://player.vimeo.com/video/111642278?color=fff600&title=0&byline=0&portrait=0" width="1180" height="664" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
   </div>
-  <?php
-  require ('car.php');
-  ?>
+
   <div>
     <?php
     require('_footer.php');
